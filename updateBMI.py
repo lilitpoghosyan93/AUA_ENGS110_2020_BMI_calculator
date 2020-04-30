2ae4cba819941627822efcd0ab7af20bdeb03c25
lilitpoghosyan93 <61308901+lilitpoghosyan93@users.noreply.github.com>
2020-04-30 19:36:08 +0400

    Update and rename BMI.py to updateBMI.py

--- updateBMI.py
+++ updateBMI.py
@@ -1,9 +1,32 @@
 import json
 
+def printComparison(oldBMI, bmi):
+    if (oldBMI != None):
+        if oldBMI > bmi:
+            print('You decrease your BMI rate')
+        elif oldBMI < bmi:
+            print('You increase your BMI rate')
+        else:
+            print('You keep your BMI rate')
+    return
+    
+    
+def printResult(bmi):
+    if bmi < 18.5:
+        print('You are underweight')
 
+    elif bmi > 18.5 and bmi < 24.9:
+        print('You are normal')
+
+    elif bmi > 25 and bmi < 29.9:
+        print('You are overweight')
+
+    else:
+        print('You are obese')
+    return
 
 def bmi_calc():
-    f = open("data.json")
+    f = open("data.JSON")
     info = json.load(f)
     f.close()
 
@@ -33,30 +56,18 @@ def bmi_calc():
             info.append(new_data)
 
 
-    with open("data.json", 'w') as f:
+    with open("data.JSON", 'w') as f:
         json.dump(info, f)
 
-    if bmi < 18.5:
-        print('You are underweight')
-
-    elif bmi > 18.5 and bmi < 24.9:
-        print('You are normal')
-
-    elif bmi > 25 and bmi < 29.9:
-        print('You are overweight')
-
-    else:
-        print('You are obese')
-
-    if (oldBMI != None):
-        if oldBMI > bmi:
-            print('You decrease your BMI rate')
-        elif oldBMI < bmi:
-            print('You increase your BMI rate')
-        else:
-            print('You keep your BMI rate')
+    printResult(bmi)
+        
+    printComparison(oldBMI, bmi)
+            
+    return
 
 
+     
+            
 def mainFunc():
     print('Welcome to the BMI system')
     bmi_calc()
@@ -64,9 +75,6 @@ def mainFunc():
     while yes_no != 'n':
         bmi_calc()
         yes_no = input('Do you want to continue?[y/n]: ')
-
-
-
-
+    return
 mainFunc()
-
+  
